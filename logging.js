cd06ced0886abdb489616fc12d9e28b365102b48
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-03 21:59:54 +0200

    select element on page

---
+++ logging.js
@@ -0,0 +1,7 @@
+const divWithId = document.getElementById("div-id");
+divWithId.style.color = 'orange'
+
+let divWithClass = Array.from(document.getElementsByClassName('div-class'))
+
+divsWithClassArray.forEach(n => (n.style.color = 'blue'))
+divsWithClassArray[2].style.color = 'black'
