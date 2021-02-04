00b74073a31140d431a481738464ca38ed03a551
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-04 21:27:03 +0200

    added event listeners js

--- EventListeners.js
+++ EventListeners.js
@@ -1,7 +1,28 @@
+// const btn = document.querySelector('[data-button]');
 
 
-const btn = document.querySelector('[data-button]');
-btn.addEventListener('click', () =>
-{
-    console.log('clicked')
-})
+// function printClick()
+// {
+//     console.log('clicked')
+// }
+
+// btn.removeEventListener('click', printClick)
+
+
+// btn.addEventListener('click', e =>
+// {
+//     console.log(e.target)
+// })
+
+// const input = document.querySelector('[data-input-text]')
+// input.addEventListener('input', e =>
+// {
+//     console.log(e.target.value === '')
+// })
+
+// const form = document.querySelector('[data-form]')
+// form.addEventListener('submit', e =>
+// {
+//     e.preventDefault()
+//     console.log('submitted form')
+// })
