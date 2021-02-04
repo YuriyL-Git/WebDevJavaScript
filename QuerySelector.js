d05dbd69259e383c21812582a1da6c8bf702d3c9
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-04 20:34:02 +0200

    event listener

---
+++ QuerySelector.js
@@ -0,0 +1,19 @@
+const dataAttributeElement = document.querySelector('[data-test]')
+const divWithClasses = document.querySelectorAll('.div-class')
+divWithClasses.forEach(d => d.style.color = 'red')
+console.log(dataAttributeElement)
+
+const input = document.querySelector('input')
+console.log(input)
+
+
+
+
+
+
+
+
+
+
+
+
